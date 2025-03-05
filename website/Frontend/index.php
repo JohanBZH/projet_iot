f737a51e1964689b253db160e0eb69ddeb6a1c31
Yoann Meynsan <yoann.meynsan@viacesi.fr>
2025-03-05 11:42:01 +0100

    fusion index.php

--- website/Frontend/index.php
+++ website/Frontend/index.php
@@ -3,10 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project iot</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Station météo</title>
 </head>
 <body>
-    <h1>Yohan sont bg</h1>
+    <header>
+        <div id="header">
+        <h1>Bienvenue sur la micro-station météo du CESI de Brest</h1>
+        </div>
+    </header>
+    <main>
+        <div id="container weather">
+            <div class="horizontal" >
+                <div class="emoji-container">
+                    <img src="https://emojitool.com/img/google/15.1/15.1-2762.png" alt="" id="snowyemoji" class="emoji">
+                    <!-- <img src="" alt="https://emojitool.com/img/google/15.1/15.1-1446.png"  id="rainyemoji" class="emoji">
+                    <img src="https://emojitool.com/img/google/15.1/15.1-339.png" alt=""  id="sunnyemoji" class="emoji">
+                    <img src="" alt="https://emojitool.com/img/messenger/1.0/cloud-5505.png"  id="cloudyemoji" class="emoji">   -->
+                </div>
+                <h2>Affichage live / graphs sur les 10 dernières min</h2>
+                <div class="horizontal" id="recentTable">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th class="headerTime">Heure</th>
+                                <th class="headerTemp">Température</th>
+                                <th class="headerXPTemp">Température <br>ressentie</th>
+                                <th class="headerHumidity">Humidité</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="rowTime">14h</td>
+                                <td data-label="rowTemp">15</td>
+                                <td data-label="rowXPTemp">13</td>
+                                <td data-label="rowHumidity">24</td>
+                            </tr>
+                            <tr>
+                                <td data-label="rowTime">14h</td>
+                                <td data-label="rowTemp">15</td>
+                                <td data-label="rowXPTemp">13</td>
+                                <td data-label="rowHumidity">24</td>
+                            </tr>
+                            <tr>
+                                <td data-label="rowTime">14h</td>
+                                <td data-label="rowTemp">15</td>
+                                <td data-label="rowXPTemp">13</td>
+                                <td data-label="rowHumidity">24</td>
+                            </tr>
+                            <tr>
+                                <td data-label="rowTime">14h</td>
+                                <td data-label="rowTemp">15</td>
+                                <td data-label="rowXPTemp">13</td>
+                                <td data-label="rowHumidity">24</td>
+                            </tr>
+                            <tr>
+                                <td data-label="rowTime">14h</td>
+                                <td data-label="rowTemp">15</td>
+                                <td data-label="rowXPTemp">13</td>
+                                <td data-label="rowHumidity">24</td>
+                            </tr>
+                            </tbody>
+                    </table>
+                </div>
+                <div class="graph">
+                    <img src="https://weather-and-climate.com/uploads/average-rainfall-france-brest-fr.png" alt="">
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <a href="https://github.com/JohanBZH/">Johan Mons</a>
+        <a href="https://github.com/MarieEustace">Marie Eustace</a>
+        <a href="https://github.com/yoannmey/">Yoann Meynsan</a>
+    </footer>
+
     <?php
 
     include '../Backend/functions.php';
@@ -38,7 +109,7 @@
         die("Une erreur s'est produite.");
     }
     ?>
-    
+    <script src="script.js"></script>
 </body>
 </html>
 
