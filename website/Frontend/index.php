bd6386164d9a03ecee358a4d9ae2e8a4a9a386cb
JohanBZH <m.johan.mons@gmail.com>
2025-03-05 14:05:13 +0100

    Pushed on alwaysdata

--- website/Frontend/index.php
+++ website/Frontend/index.php
@@ -84,19 +84,23 @@
 
     include '../Backend/vendor/autoload.php';
 
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv = Dotenv\Dotenv::createImmutable('/home/jomayo/www/Frontend'); //
     $dotenv->load();
+
+    $deb=0;
 
     try {
 
         $dsn = "mysql:host={$_ENV['MYSQL_HOST']};dbname={$_ENV['MYSQL_DATABASE']};charset=utf8mb4";
         
+$deb=1;
+
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
         ];
-
+$deb=2;
         $db = new PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $options);
 
         //insertUser(<'login'>,<'password'>,$db);
@@ -106,6 +110,8 @@
 
     } catch(PDOException $e) {
         error_log("Database error: " . $e->getMessage());
+
+        echo $deb;
         die("Une erreur s'est produite.");
     }
     ?>
