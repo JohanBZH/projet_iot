33054e2be9b7e8d591a411b43dbec879878995dd
Marie Eustace <marie.eustace@viacesi.fr>
2025-03-11 00:42:00 +0100

    added db_conn.php for pdo and function to insert new user in function.php

--- website/Frontend/index.php
+++ website/Frontend/index.php
@@ -1,44 +1,7 @@
 <?php
-include '../Backend/functions.php';
-include '../Backend/vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable('/home/jomayo/www/Frontend');
-$dotenv->load();
-
-$deb = 0;
-
-$temperature = isset($_GET['temperature']) ? $_GET['temperature'] : null;
-$humidity = isset($_GET['humidity']) ? $_GET['humidity'] : null;
-$time_stamp = date('Y-m-d H:i:s');
-
-try {
-    $dsn = "mysql:host={$_ENV['MYSQL_HOST']};dbname={$_ENV['MYSQL_DATABASE']};charset=utf8mb4";
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ];
-
-    $db = new PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $options);
-
-    if ($temperature !== null && $humidity !== null && is_numeric($temperature) && is_numeric($humidity)) {
-        $floatTemperature = (float)$temperature;
-        $floatHumidity = (float)$humidity;
-
-        $floatTemperature = $floatTemperature/100;
-        $floatHumidity = $floatHumidity/100;
-
-        $stmt = $db->prepare("INSERT INTO Data (Temperature_value, Humidity_value, Time_stamp) VALUES (:temperature, :humidity, :time_stamp)");
-        $stmt->bindParam(':temperature', $floatTemperature);
-        $stmt->bindParam(':humidity', $floatHumidity);
-        $stmt->bindParam(':time_stamp', $time_stamp);
-        $stmt->execute();
-    }
-
-} catch(PDOException $e) {
-    error_log("Database error: " . $e->getMessage());
-    echo "Erreur de la base de données : " . $e->getMessage() . "<br>";
-}
+include '../Backend/db_conn.php';
+// include '../Backend/functions.php';
+// include '../Backend/vendor/autoload.php';
 ?>
 
 <!DOCTYPE html>
